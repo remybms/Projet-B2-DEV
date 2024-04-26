84d0e6947bde0cdfba788f8c73f828952e6b7db6
remybms <bamasremy43@gmail.com>
2024-04-26 08:37:47 +0200

    testing files

--- app/page.js
+++ app/page.js
@@ -1,22 +1,27 @@
 "use client"
 import React from "react";
+import { hashPassword } from "@/components/password";
 
 export default function Home() {
 
-  const [content, setContent] = React.useState('')
+  const [username, setUsername] = React.useState('')
+  const [name, setName] = React.useState('')
+  const [surname, setSurname] = React.useState('')
+  const [mail, setMail] = React.useState('')
+  const [password, setPassword] = React.useState('')
 
   async function sendData(e) {
     e.preventDefault();
     var data
     try {
-        const response = await fetch('/api/posts', {
+        const response = await fetch('/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                "idUser": 1,
-                "content": content,
+                "mail" : mail,
+                "password" : hashPassword(password),
                 }),
         });
 
@@ -30,7 +35,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white text-black">
       <form onSubmit={sendData}>
-        <input type="text" placeholder="content" onChange={(e) => setContent(e.target.value)}/>
+        <input type="email" placeholder="email" onChange={(e) => setMail(e.target.value)}/>
+        <input type="text" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
         <button type="submit">envoyer</button>
       </form>
       
