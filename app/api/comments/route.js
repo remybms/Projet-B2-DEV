a1716571d17863aa893b3240b1239e1056344036
remybms <bamasremy43@gmail.com>
2024-04-26 08:36:17 +0200

    comments route

--- app/api/comments/route.js
+++ app/api/comments/route.js
@@ -1,7 +1,8 @@
 import sqlite3 from "sqlite3";
-import { open, Database } from "sqlite";
+import { open } from "sqlite";
+import { NextRequest, NextResponse } from "next/server";
 
-let db = null;
+let db
 
 export async function GET(req, res) {
   if (!db) {
@@ -13,8 +14,23 @@ export async function GET(req, res) {
 
   const items = await db.all("SELECT * FROM comments");
 
-  return new Response(JSON.stringify(items), {
-    headers: { "Content-Type": "application/json" },
-    status: 200,
-  });
+  return NextResponse.json(items, { status: 200 });
+}
+
+export async function POST(req, res) {
+  if (!db) {
+    db = await open({
+      filename: "./collection.db",
+      driver: sqlite3.Database,
+    });
+  }
+
+  const data = await req.json();
+
+  db.run('INSERT INTO comments (idUser, content, idPost) VALUES (?, ?, ?)', [data.idUser, data.content, data.idPost], (err) => {
+    if (err) {
+      return NextResponse.json({ error: err.message }, { status: 500 });
+    }
+  })
+  return NextResponse.json({ message: 'Données insérées avec succès' }, { status: 200 });
 }
